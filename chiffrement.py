91dd55cbbd385d4234b962b2ae1dd82f6c3b44b7
HyouKa'sh <71031336+HyouKash@users.noreply.github.com>
2021-03-22 11:51:24 +0100

    Update chiffrement.py

--- chiffrement.py
+++ chiffrement.py
@@ -14,5 +14,5 @@ def dechiffre(message_chiffre, masque):
         new_message.append(chr(a^ord(b)))
     return new_message
         
-print(chiffre("Salut ca va", "CETTEPHRASEESTVRAIMENTTRESTRESLONGUEMAISCESTFAITEXPRES"))
-print(dechiffre("[16, 36, 56, 33, 49, 112, 175, 51, 97, 37, 36]", "CETTEPHRASEESTVRAIMENTTRESTRESLONGUEMAISCESTFAITEXPRES"))
+print(chiffre("Salut ca va", "AMASQUEVERYLONGBUTITSNORMAL"))
+print(dechiffre("[16, 36, 56, 33, 49, 112, 175, 51, 97, 37, 36]", "AMASQUEVERYLONGBUTITSNORMAL"))
