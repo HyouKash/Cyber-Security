0f3decab4bd8ae96761a58cc939a6ef62c964ea2
HyouKa'sh <71031336+HyouKash@users.noreply.github.com>
2021-03-22 11:50:41 +0100

    Symmetric encryption.py
    Symmetric encryption

---
+++ chiffrement.py
@@ -0,0 +1,18 @@
+def chiffre(message, masque):
+    new_message = []
+    for i in range(len(message)):
+        a = message[i]
+        b = masque[i]
+        new_message.append(ord(a)^ord(b))
+    return new_message
+
+def dechiffre(message_chiffre, masque):
+    new_message = []
+    for i in range(len(message_chiffre)):
+        a = message_chiffre[i]
+        b = masque[i]
+        new_message.append(chr(a^ord(b)))
+    return new_message
+        
+print(chiffre("Salut ca va", "CETTEPHRASEESTVRAIMENTTRESTRESLONGUEMAISCESTFAITEXPRES"))
+print(dechiffre("[16, 36, 56, 33, 49, 112, 175, 51, 97, 37, 36]", "CETTEPHRASEESTVRAIMENTTRESTRESLONGUEMAISCESTFAITEXPRES"))
